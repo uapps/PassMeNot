efed9ef4abe73ffdeb8eb21193c84dd3ac40edac
Max Hoffmann <tsov@me.com>
2014-06-16 15:39:28 +0200

    don't select tile if its already selected

--- js/directives/tiles.js
+++ js/directives/tiles.js
@@ -13,20 +13,26 @@ angular.module('PassMeNot.directives.tiles', [])
 		  controller: [ '$scope', '$element', function($scope, $element) {
 			  var tiles = $scope.tiles = [ ]
 			  this.addTile = function(tile) {
-				  if (tiles.length == 0) tile.selected = true
+				  if (tiles.length == 0) $scope.select(tile)
 				  tiles.push(tile)
 			  }
+              $scope.select = function(tile) {
+                  $scope.selectedTile = tile
+                  tile.selected = true
+              }
 			  $scope.deselectTiles = function() {
 				  angular.forEach($scope.tiles, function(tile) {
 					  if (tile.selected) tile.selected = false
 				  })
 			  }
 			  $scope.toggle = function(index) {
+                  if ($scope.tiles[index] == $scope.selectedTile) return
 				  var element = angular.element($element).addClass($scope.animation)
 				  $timeout(function() {
 					  $timeout(function() {element.removeClass($scope.animation + ' ' + $scope.animation + '-back')}, 125)
 					  $scope.deselectTiles()
                       $scope.tiles[index].selected = true
+                      $scope.select($scope.tiles[index])
 					  element.addClass($scope.animation + '-back')
 				  }, 125)
 			  }
